df5caa7526e657d262685ae39c2e32b3b2f7c720
Pasha <bayntis@gmail.com>
2020-11-21 13:04:59 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+" use strict ";
+
+const user = "Ivan";
+alert(`Privet, ${user}`);
+
+
+let incr = 10,
+    decr = 10;
+
+   // ++incr;
+   // --decr;
+    console.log(incr++);
+    console.log(decr--);
+
+    console.log(5 % 2);
+
+
+
+    console.log(2 + 2 * 2 != "6");
+
+const isChecked = true,
+      isClose = false;
+
+      console.log(isChecked || isClose);
+
+
