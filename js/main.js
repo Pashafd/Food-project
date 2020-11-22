2b40f97332cde0d80e17247a2cfd9a6f2dcddad6
Pasha <Bayntis@gmail.com>
2020-11-22 14:47:38 +0200

    homework episode 15

--- js/main.js
+++ js/main.js
@@ -1,30 +1,36 @@
-" use strict ";
+'use strict';
 
-let numberOfFilms = +prompt("skolko filmov yje posmotreli");
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-let personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
+ 
+for (let i = 0; i < 2; i++) {
+     const a = prompt("skoko filmov"),
+     b = prompt('ocenochka');
 
+ if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+     personalMovieDB.movies[a] = b;
 
-let questionOne = prompt("Odin iz poslednih posmotrenuh filmov?");
-let questionTwo = +prompt("Na skolko ocenite ego");
-let questionThre = prompt("Odin iz poslednih posmotrenuh filmov?");
-let questionFour = +prompt("Na skolko ocenite ego");
-
-personalMovieDB.movies[questionOne] = questionTwo;
-personalMovieDB.movies[questionThre] = questionFour;
+ } else {
+     console.log('error');
+     i--;
+ }
+}
 
 console.log(personalMovieDB);
-   
 
-
-
-
-
-
-
+if (personalMovieDB.count <= 10) {
+    console.log('prosmotreno dovolno malo filmov');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    console.log('Vu klasnuy zritel');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Vu kinoman!');
+} else {
+    console.log('Error!!');
+}
