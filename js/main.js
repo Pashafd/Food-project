5fb9ea7ac060c4eb4f1fe771ae63e696852e02ca
Pasha <Bayntis@gmail.com>
2020-11-25 22:45:10 +0200

    homework ep24

--- js/main.js
+++ js/main.js
@@ -1,30 +1,19 @@
 " use strict ";
-
-let numberOfFilms,
-    typesFilm = [];
- 
-// функція старт питає. та не дає юзеру не дати відповіть на питання
-function start() {
-  numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-            while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-              numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-  }
-}
-
-
- start();
-
+let typesFilm = [];
   const personalMovieDB = {
-  count: numberOfFilms,
+  count: 0,
   movies: {},
   actors: {},
   genres: typesFilm,
-  privat: false
-};
+  privat: false,
+  start: function () {
+    personalMovieDB.count = +prompt('skolko filmov posmotrel', "");
 
-// функція записує який фільм та його оцінку в константу .movies
-function rememberMyFilms() {
+    while (personalMovieDB.count == "" || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+      personalMovieDB.count = +prompt('skolko filmov posmotrel', "");
+    }
+  },
+  rememberMyFilms: function () {
     for (let i = 0; i < 2; i++) {
   const a = prompt("kakoy film"),
   b = prompt('ocenochka');
@@ -37,44 +26,43 @@ if (a != null && b != null && a != '' && b != '' && a.length < 50) {
   i--;
     }
    }
-}
-
-
-
-// питає юзера про жанри кіно, іттерація 3 рази
-function writeYourGenres () {
+},
+writeYourGenres: function() {
   for (let i = 0; i < 3; i++) {
-  typesFilm[i] = prompt(`Vah lybimuy janr pid nomerom ${i + 1} `);
+    let genre = prompt(`Vah lybimuy janr pid nomerom ${i + 1}`);
+    if(genre === "" || genre == null) {
+      console.log("vu veli ne korektnue danni")
+      i--;
+    }else {
+      personalMovieDB.genres[i] = genre;
+    }
+    }
+    personalMovieDB.genres.forEach((item, i) => {
+      console.log(`lubimuy janr ${i + 1} - eto ${item}`);
+    })
+    },
+toggleVisibleMyDB: function() {
+      if (personalMovieDB.privat) {
+        personalMovieDB.privat = false;
+      }else {
+        personalMovieDB.privat = true;
+      }
+},
+detectPersonalLvl: function () { 
+  if (personalMovieDB.count <= 10) {
+    console.log('prosmotreno dovolno malo filmov');
+  } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    console.log('Vu klasnuy zritel');
+  } else if (personalMovieDB.count >= 30) {
+    console.log('Vu kinoman!');
+  } else {
+    console.log('Error!!');
   }
-return (typesFilm);
-}
+    },
+    showMyDB: function (hidden) {
+       if (!hidden) {
+         console.log(personalMovieDB);
+       }
+    }
 
-
-
-
-
-// фунція дає оцінку який же ти переглядач фільмів)
-function detectPersonalLvl() { 
-if (personalMovieDB.count <= 10) {
-  console.log('prosmotreno dovolno malo filmov');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-  console.log('Vu klasnuy zritel');
-} else if (personalMovieDB.count >= 30) {
-  console.log('Vu kinoman!');
-} else {
-  console.log('Error!!');
-}
-  }
-
-
-
-// функція яка запускає весь код якщо .privat - не правда
-function showMyDB() {
-  if (personalMovieDB.privat == false) {
-    detectPersonalLvl(),
-    writeYourGenres(),
-    rememberMyFilms();
-  }
-}
-showMyDB();
-console.log(personalMovieDB);
+};
